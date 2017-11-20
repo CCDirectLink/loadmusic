adb95c38b409d15d37b30073184edef9ac5f9b2f
ac2pic <ac2pic@gmail.com>
2017-11-20 12:35:45 -0600

    Updated the code to account for CCLoader definitions update

--- loadmusic/mod.js
+++ loadmusic/mod.js
@@ -1,27 +1,38 @@
-var modDir = "assets/mods/custommusic/";
-function loadCustomMusic() {
-	var musicData = JSON.parse(fs.readFileSync(modDir + "cm.db", "utf8"));
-	for(var i in musicData) {
-		for(var j in musicData[i]) {
-			musicData[i][cc.bgm[j]] = musicData[i][j];
-			if(cc.bgm[j] !== j)
-				delete musicData[i][j];
-		}
-	}
-	ig.merge(ig[cc.bgm.playlist], musicData);
+var modDir = "assets/mods/loadmusic/";
+Object.entries = Object.entries || function(obj) {
+    var pairs = []
+    for (var i in obj) {
+        pairs.push([i, obj[i]])
+    }
+    return pairs
 };
+Object.rename = function(obj, oldName, newName) {
+    if (obj[oldName]) {
+        obj[newName] = obj[oldName]
+        delete obj[oldName]
+    }
+}
+
+function loadCustomMusic() {
+    var musicData = JSON.parse(fs.readFileSync(modDir + "cm.db", "utf8"));
+    var musicKeys = Object.entries(cc.ig.bgm.varNames);
+    musicKeys.forEach(function(element) {
+        for (var i in musicData) {
+            Object.rename(musicData[i], element[0], element[1])
+        }
+    })
+    ig.merge(cc.ig.BGM_TRACK_LIST, musicData);
+};
+
 function loadCustomTrackConfig() {
-	var mapMusicData = JSON.parse(fs.readFileSync(modDir + "mm.db", "utf8"));
-	for(var mapConfigName of mapMusicData) {
-		for(var musicConfigType of mapConfigName) {
-			for(var i in musicConfigType) {
-				musicConfigType[cc.bgm[i]] = musicConfigType[i];
-				if(cc.bgm[i] !== i) 
-					delete mapMusicData[mapConfigName][musicConfigType][i];
-			}
-		}
-	}
-	ig.merge(ig[cc.bgm.mapConfig], mapMusicData);
+    var mapMusicData = JSON.parse(fs.readFileSync(modDir + "mm.db", "utf8"));
+    for (var mapName in mapMusicData) {
+        var mapBGMData = mapMusicData[mapName];
+        for (var themeType in mapBGMData) {
+            Object.rename(mapBGMData[themeType], "name", cc.ig.varNames.BGMpath)
+        }
+    }
+    ig.merge(cc.ig.bgm.mapConfig, mapMusicData);
 };
 loadCustomMusic()
 loadCustomTrackConfig()
