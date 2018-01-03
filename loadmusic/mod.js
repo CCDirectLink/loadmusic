a49601f4872bcdcc1879ab940ba60bbbab3d64b7
ac2pic <ac2pic@gmail.com>
2018-01-03 17:52:12 -0600

    Added Voice Acting capability

--- loadmusic/mod.js
+++ loadmusic/mod.js
@@ -13,7 +13,63 @@ Object.rename = function(obj, oldName, newName) {
         delete obj[oldName]
     }
 }
-
+function _extendProto(obj, name, newFunc) {
+	let old = obj.prototype[name];
+	obj.prototype[name] = function() {
+	    newFunc.apply(this, arguments);
+		old.apply(this, arguments);
+	}
+}
+function extendFunc(name, newFunc) {
+     //we can assume the oldFunc is the original function
+	 if(typeof(name) === "function") name = name.name; 
+	 else if(typeof this[name] === "object" && this[name].name) name = this[name].name;
+	 var oldFunc = this[name];
+	 _this = this;
+	 this[name] = function() {
+		newFunc.apply(_this, arguments);
+		oldFunc.apply(_this, arguments);
+	 }
+}
+function loadVoiceActing() {
+	var voice_database = JSON.parse(fs.readFileSync(modDir + "va.db", 'utf8'));
+	var silence = new cc.ig.Effects.PLAY_SOUND(null, {
+		group : "va",
+		global : true,
+		sound : "mods/loadmusic/va/silence.ogg"
+	});
+	for(var mapName in voice_database) {
+		for(var langUid in voice_database[mapName]) {
+			voice_database[mapName][langUid] = new cc.ig.Effects.PLAY_SOUND(null, {
+				group : "va",
+				global : true,
+				sound : voice_database[mapName][langUid]
+			});
+		}
+	}
+	console.log("Finished loading voices");
+	var map_va_templates = null;
+	extendFunc.apply(cc.ig.gameMain, [cc.ig.gameMain.LoadMap, function(map_path) {
+		map_va_templates = voice_database[map_path] || {};
+	}]);
+	var langUid = cc.ig.LangLabel.varNames.langUid;
+	var msg_langUid;
+	var voice_sound;
+	var lastLangUid = -1;
+	var isPlaying = false;
+	_extendProto(cc.ig.events.SHOW_MSG, "start", function() { 
+		msg_langUid = this.message[langUid];
+		voice_sound = map_va_templates[msg_langUid.toString()];
+		if(voice_sound) {
+			isPlaying = true;
+			voice_sound.start();
+		} else if(isPlaying) {
+			isPlaying = false;
+			silence.start();
+		}
+		lastLangUid = msg_langUid.toString();
+	});
+}
 function loadCustomMusic() {
     var musicData = JSON.parse(fs.readFileSync(modDir + "cm.db", "utf8"));
     var musicKeys = Object.entries(cc.ig.bgm.varNames);
@@ -35,5 +91,15 @@ function loadCustomTrackConfig() {
     }
     ig.merge(cc.ig.bgm.mapConfig, mapMusicData);
 };
-loadCustomMusic()
-loadCustomTrackConfig()
+
+try {
+	loadCustomMusic();
+}catch(e) {}
+
+try {
+	loadCustomTrackConfig();
+}catch(e) {}
+
+try {
+	loadVoiceActing();	
+}catch(e) {}
